e66a1d0df2f99273544d0bc2fa7e58cb82b271df
Fabian Bliem <fabianbliem@icloud.com>
2021-06-08 18:36:40 +0200

    feat: added EMG

--- osc_data_stream.ino
+++ osc_data_stream.ino
@@ -1,24 +1,28 @@
 #include <Arduino.h>
+#include <OSCMessage.h>
 
-int message = 0;     //  This will hold one byte of the serial message
-int redLEDPin = 13;   //  What pin is the red LED connected to?
-int redLED = 0;          //  The value/brightness of the LED, can be 0-255
+int curVal;
+int prevVal=0;
+int EMGPin = A1;
+int EMGVal = 0;
 
 void setup() {
-    Serial.begin(9600);  //set serial to 9600 baud rate
+    Serial.begin(115200);
 }
 
 void loop(){
-    if (Serial.available() > 0) { //  Check if there is a new message
-        message = Serial.read();    //  Put the serial input into the message
 
-        if (message == 'R'){  //  If a capitol R is received...
-            redLED = 255;       //  Set redLED to 255 (on)
-        }
-        if (message == 'r'){  //  If a lowercase r is received...
-            redLED = 0;         //  Set redLED to 0 (off)
-        }
+    EMGVal = analogRead(EMGPin);
+    curVal=EMGVal;
 
+    char buff[16];
+    sprintf(buff, "/emg/%d", curVal);
+    OSCMessage msg(buff);
+
+    if (curVal != prevVal) {
+        msg.add(curVal)
+        msg.send(Serial);
+        prevVal=curVal;
     }
-    analogWrite(redLEDPin, redLED);  //  Write an analog value between 0-255
+    delay(20);
 }
